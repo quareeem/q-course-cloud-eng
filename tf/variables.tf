7d190463985787a836dd9b895fa73ae529d9feee
Karim Akhmediyev <karim.akhmediyev@quantori.com>
2024-05-14 20:12:58 +0500

    fix: fmt

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,25 +1,25 @@
 variable "vpc_id" {
   description = "The VPC ID where resources will be created."
   type        = string
-  default = "vpc-024cf058980b63412"
+  default     = "vpc-024cf058980b63412"
 }
 
 variable "subnet_id" {
   description = "The Subnet ID where the EC2 instance will be created."
   type        = string
-  default = "subnet-07549c87757e073ea"
+  default     = "subnet-07549c87757e073ea"
 }
 
 variable "allowed_ssh_cidr" {
   description = "The CIDR block allowed for SSH access."
   type        = string
-  default = "147.161.130.205/32"
+  default     = "147.161.130.205/32"
 }
 
 variable "bucket_name" {
   description = "The name of the S3 bucket to create."
   type        = string
-  default = "qnt-bucket-karim-akhmediyev"
+  default     = "qnt-bucket-karim-akhmediyev"
 }
 
 variable "ami_name" {
